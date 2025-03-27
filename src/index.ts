c7e55b855c7ea0b47a75e32a67c3779b4d672a27
liufei <poticr@gmail.com>
2025-03-27 13:20:05 +0800

    Better display

--- src/index.ts
+++ src/index.ts
@@ -8,25 +8,29 @@ const rl = readline
   .createInterface({ input, output })
   .on('SIGINT', () => rl.close())
 
-async function run() {
+async function main() {
   const sentence = await rl.question('Sentence: ')
-  const word = await rl.question('Word: ')
-
   console.log()
+
+  const word = await rl.question('Word: ')
+  console.log()
+
   console.log('Thinking...')
   console.log()
 
   const meaning = await ai(sentence, word)
-  let [partOfSpeech, definition] = meaning.split('\n')
+  const [partOfSpeech, definition] = meaning.split('\n')
+
   console.log(`Part of Speech: ${partOfSpeech}`)
+  console.log()
+
   console.log(`Definition: ${definition}`)
   console.log()
 
-  const noteId = await addNote(sentence, word, partOfSpeech, definition)
-  if (noteId) console.log(`Node ID: ${noteId}`)
+  await addNote(sentence, word, partOfSpeech, definition)
   console.log()
 
-  run()
+  main()
 }
 
-run()
+main()
